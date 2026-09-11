530ad00f262a7d97bad34e0e6adaab5139b733fc
Javad <smjh9386hosseini@gmail.com>
2026-09-11 11:50:13 +0330

    fix(wsgi): support running passenger_wsgi.py from either parent directory or project root

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,17 @@
 import os
 import sys
 
-# Add project root directory to Python path
+# Support running either from ~/mahta-cycle/ or directly inside ~/mahta-cycle/scooter_website/
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-if CURRENT_DIR not in sys.path:
-    sys.path.insert(0, CURRENT_DIR)
+SUB_PROJECT = os.path.join(CURRENT_DIR, 'scooter_website')
+
+if os.path.isdir(SUB_PROJECT):
+    PROJECT_DIR = SUB_PROJECT
+else:
+    PROJECT_DIR = CURRENT_DIR
+
+if PROJECT_DIR not in sys.path:
+    sys.path.insert(0, PROJECT_DIR)
 
 # Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
